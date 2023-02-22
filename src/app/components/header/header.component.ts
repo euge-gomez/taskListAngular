1b84adc3c67c7bd4726a7590dda181fd8c21eee2
Euge <eugequimica@gmail.com>
2023-02-22 07:57:57 -0300

    Primer boton

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +11,9 @@ export class HeaderComponent {
   constructor() {}
 
   ngOnInit(): void {
-    
+  }
+
+  toggleAddTask(){
+    console.log("toggleAddTask!")
   }
 }
