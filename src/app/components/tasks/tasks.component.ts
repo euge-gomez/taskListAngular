3ba0dbe4cbe4be1948aa16171ceaa37a8adadea9
Euge <eugequimica@gmail.com>
2023-02-23 11:06:43 -0300

    Add Task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -30,4 +30,9 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder
     this.taskService. updateTaskReminder(task).subscribe();
   }
+  addTask(task:Task) {
+    this.taskService.addTask(task).subscribe((task)=> (
+      this.task.push(task)
+    ))
+  }
 }
